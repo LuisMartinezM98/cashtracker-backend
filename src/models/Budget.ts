19e7d3fbf74f0176ab7dc85f822e18cafca54d65
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-05-12 22:13:45 -0600

    feat(budget): add new functions to Model and declare ForeignKey and BelongsTo attributes

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -1,24 +1,43 @@
-import { Table, Column, DataType, HasMany, BelongsTo, ForeignKey, Model } from 'sequelize-typescript';
-import Expense from './Expense';
+import {
+  Table,
+  Column,
+  DataType,
+  HasMany,
+  BelongsTo,
+  ForeignKey,
+  Model,
+  AllowNull,
+} from "sequelize-typescript";
+import Expense from "./Expense";
+import User from "./User";
 
 @Table({
-    tableName: 'budgets'
+  tableName: "budgets",
 })
-class Budget extends Model{
-    @Column({
-        type: DataType.STRING(100)
-    })
-    declare name: string;
-    @Column({
-        type: DataType.DECIMAL
-    })
-    declare amount: number;
+class Budget extends Model {
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING(100),
+  })
+  declare name: string;
 
-    @HasMany(() => Expense, {
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-    })
-    declare expenses: Expense[];
+  @AllowNull(false)
+  @Column({
+    type: DataType.DECIMAL,
+  })
+  declare amount: number;
+
+  @HasMany(() => Expense, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  declare expenses: Expense[];
+
+  @ForeignKey(() => User)
+  declare userId: number;
+
+  @BelongsTo(() => User)
+  declare user: User;
 }
 
 export default Budget;
