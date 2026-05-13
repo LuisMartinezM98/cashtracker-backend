cc18fadf10e866566c15e434f240fa4cee82f7bd
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-05-12 22:11:16 -0600

    feat: add AuthEmail with sendConfirmation method

---
+++ src/emails/AuthEmail.ts
@@ -0,0 +1,25 @@
+import { transport } from "../config/nodemailer";
+
+type EmailType = {
+  name: string;
+  email: string;
+  token: string;
+};
+
+export class AuthEmail {
+  static sendConfirmation = async (user: EmailType) => {
+    const email = await transport.sendMail({
+      from: "CashTracker <admin@cashtrackr.com>",
+      to: user.email,
+      subject: "CashTrackr - Confirma tu cuenta",
+      html: `
+                <h1>Hola ${user.name}</h1>
+                <p>Gracias por registrarte en CashTrackr.</p>
+                <p>Para confirmar tu cuenta, haz clic en el siguiente enlace:</p>
+                <a href="#">Confirmar cuenta</a>
+                <p>e ingresa el código: <b>${user.token}</b></p>
+                `,
+    });
+    console.log('Email enviado', email.messageId);
+  };
+}
