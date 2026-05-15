c8901833a8d273292565c923198686366a14cfac
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-05-14 22:27:25 -0600

    feat: budget middleware with new errors

--- src/middleware/budget.ts
+++ src/middleware/budget.ts
@@ -3,44 +3,76 @@ import { body, param, validationResult } from "express-validator";
 import Budget from "../models/Budget";
 
 declare global {
-    namespace Express {
-        interface Request {
-            budget?: Budget;
-        }
+  namespace Express {
+    interface Request {
+      budget?: Budget;
     }
+  }
 }
 
-export const validateBudgetId = async (req: Request, res: Response, next: NextFunction) => {
-    await param('budgetId')
-        .isInt().withMessage('Invalid ID')
-        .custom(value => value > 0).withMessage('Invalid ID').run(req);
-    let errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+export const validateBudgetId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  await param("budgetId")
+    .isInt()
+    .withMessage("Invalid ID")
+    .custom((value) => value > 0)
+    .withMessage("Invalid ID")
+    .run(req);
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+export const validateBudgetExist = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { budgetId } = req.params;
+  try {
+    const budget = await Budget.findByPk(+budgetId);
+    if (!budget) {
+      const error = new Error("Budget not found");
+      return res.status(404).json({ error: error.message });
     }
+    req.budget = budget;
+
     next();
-}
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-export const validateBudgetExist = async (req: Request, res: Response, next: NextFunction) => {
-    const { budgetId } = req.params;
-    try {
-        const budget = await Budget.findByPk(+budgetId);
-        if (!budget) {
-            const error = new Error('Budget not found');
-            return res.status(404).json({ error: error.message });
-        }
-        req.budget = budget;
+export const validateBudgetInput = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  await body("name").notEmpty().withMessage("Name is required").run(req);
+  await body("amount")
+    .notEmpty()
+    .withMessage("Amount is required")
+    .isNumeric()
+    .withMessage("Amount must be a number")
+    .custom((value) => value > 0)
+    .withMessage("Amount must be greater than 0")
+    .run(req);
+  next();
+};
 
-        next();
-    } catch (error) {
-        return res.status(500).json({ error: 'Internal server error' });
-    }
-}
-
-export const validateBudgetInput = async (req: Request, res: Response, next: NextFunction) => {
-    await body('name').notEmpty().withMessage('Name is required').run(req);
-    await body('amount').notEmpty().withMessage('Amount is required')
-            .isNumeric().withMessage('Amount must be a number')
-            .custom((value) => value > 0).withMessage('Amount must be greater than 0').run(req);
-    next();
+export async function hasAcces(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (req.budget.userId !== req.user.id) {
+    const error = new Error("Unauthorized");
+    return res.status(401).json({ error: error.message });
+  }
+  next();
 }
