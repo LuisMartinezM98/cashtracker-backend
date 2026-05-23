54c083f8bc708a30cd280c7559e77ff278d49460
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-05-23 08:53:30 -0600

    fix: connectDB removed from server

--- src/server.ts
+++ src/server.ts
@@ -6,22 +6,24 @@ import budgetRouter from './routes/budgetRouter';
 import authRouter from './routes/authRouter';
 
 
-async function connectDB() {
+export async function connectDB() {
     try {
         await db.authenticate();
-        db.sync();
-        console.log(colors.green.bold('Database connected successfully'))
+        await db.sync();
+        // console.log(colors.green.bold('Database connected successfully'))
     } catch (error) {
-        console.log(colors.red.bold('Database connection failed'))
+        // console.log(colors.red.bold('Database connection failed'))
     }
 }
-
-connectDB();
 
 const app: Application = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/budgets', budgetRouter);
 app.use('/api/auth', authRouter);
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
 
 export default app;
